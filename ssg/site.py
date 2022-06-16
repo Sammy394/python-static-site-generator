9113b6dfc96faa09c80f00592283d433e2d26a2d
Sammy394 <sam.h.strother@gmail.com>
2022-06-16 13:18:23 +1000

    ssg.py/site.py changes

--- ssg/site.py
+++ ssg/site.py
@@ -1,8 +1,16 @@
-import Path from "pathlib"
+from pathlib import Path
 
-class Site(self, source, dest):
-    source = path(self)
-    dest = path(self)
+class Site:
+    def __init__(self, source, dest):
+        self.source = path(source)
+        self.dest = path(dest)
 
     def create_dir(self, path):
-        
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
