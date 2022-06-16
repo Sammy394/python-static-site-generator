e1a7bf8b6234d04f7224c3f38f5078c0c268b6fd
Sammy394 <sam.h.strother@gmail.com>
2022-06-16 15:21:31 +1000

    module5 completion

--- ssg/site.py
+++ ssg/site.py
@@ -1,3 +1,5 @@
+import sys
+
 from pathlib import Path
 
 class Site:
@@ -20,7 +22,7 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
-            print("Not Implemented")
+            print(self.error("No parser for the {} extension, file skipped!".format(path.suffix)))
 
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
@@ -28,4 +30,8 @@ class Site:
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
-                self.run_parser(path)               
+                self.run_parser(path)
+
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
